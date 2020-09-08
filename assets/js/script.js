13781b671a241667b2a436df61f97eaf18f2adab
akjmalamin7 <thpi205526@gmail.com>
2020-09-08 17:54:19 +0600

    initialize

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function(){
+
+
+    //start slick slider
+      
+        $('.home-slick-slider').slick({
+            slidesToShow: 1,
+            dots: true,
+            infinite: true,
+            speed: 700,
+            fade: true,
+            cssEase: 'linear',
+            autoplay: true,
+            autoplaySpeed: 3000,
+            dots: false
+        });
+   
+     
+});
